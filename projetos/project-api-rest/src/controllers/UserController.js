db9486e25bc55aa0f80322ef58061ead18e96a8d
Junior Alves <junioralves.jav@gmail.com>
2022-05-06 18:18:27 -0300

    In progress *[Resolve bug on update user]*

---
+++ projetos/project-api-rest/src/controllers/UserController.js
@@ -0,0 +1,62 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    try {
+      const novoUser = await User.create(req.body);
+
+      return res.json(novoUser);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+
+      return res.json(users);
+    } catch (err) {
+      console.log(err);
+      return res.json(null);
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id); // PK -> Primary Key
+
+      return res.json(user);
+    } catch (err) {
+      console.log(err);
+      return res.json(null);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['ID não enviado'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe'],
+        });
+      }
+
+      const novosDados = user.update(req.body);
+      return res.json(novosDados);
+    } catch (err) {
+      return res.json(null);
+    }
+  }
+}
+
+export default new UserController();
---
+++ projetos/project-api-rest/src/routes/userRoutes.js
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import UserController from '../controllers/UserController';
+
+const router = new Router();
+
+router.post('/', UserController.store);
+router.get('/', UserController.index);
+router.get('/:id', UserController.show);
+router.put('/:id', UserController.update);
+/**
+ * MÉTODOS DE UM ÚNICO CONTROLLER
+ *
+ * index -> lista todos os usuários -> GET
+ * store/create -> cria um novo usuário -> PUT
+ * delete -> apaga um usuário -. DELETE
+ * show -> mostra um usuário -> GET
+ * update -> atualiza um usuário -> PATCH(serve para alterar um único valor) ou PUT
+ */
+
+export default router;
