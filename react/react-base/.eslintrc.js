91ba592e5f3e8fb89f08533905e250a994563e27
Junior Alves <junioralves.jav@gmail.com>
2022-10-26 00:34:38 -0300

    Styled components has been added.

--- react/react-base/.eslintrc.js
+++ react/react-base/.eslintrc.js
@@ -3,15 +3,8 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'prettier',
-    'prettier/react',
-  ],
-  overrides: [
-  ],
-  parse: 'babel-eslint',
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  overrides: [],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -19,16 +12,12 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    'prettier',
-    'react-hooks',
-  ],
+  plugins: ['react', 'prettier', 'react-hooks'],
   rules: {
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': 0,
     'import/prefer-default-export': 0,
     'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn'
+    'react-hooks/exhaustive-deps': 'warn',
   },
 };
---
+++ react/react-base/src/pages/Login/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { Title } from './styled';
+import { Container } from '../../styles/GlobalStyles';
+
+export default function Login() {
+  return (
+    <Container>
+      <Title>
+        Login
+        <small>Oie</small>
+      </Title>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+        distinctio porro quibusdam nam aperiam ducimus modi fuga at quod.
+        Adipisci, ipsum. Nostrum veritatis necessitatibus labore atque,
+        laudantium culpa commodi voluptate?
+      </p>
+    </Container>
+  );
+}
