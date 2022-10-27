5996713ca73c6e6441e8570bbe2a688c3e610708
Junior Alves <junioralves.jav@gmail.com>
2022-10-27 14:02:11 -0300

    Protected router has been created.

--- react/react-base/src/App.js
+++ react/react-base/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import Login from './pages/Login';
+import { Router } from 'react-router-dom';
+// import Login from './pages/Login';
 
+import history from './services/history';
 import GlobalStyles from './styles/GlobalStyles';
 import Header from './components/Header';
+import Routes from './routes';
 
 function App() {
   return (
-    <>
+    <Router history={history}>
       <Header />
-      <Login />
+      <Routes />
       <GlobalStyles />
-    </>
+    </Router>
   );
 }
 
