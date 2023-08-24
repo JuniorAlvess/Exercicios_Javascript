023c4a74ece14324bbd8ef437b2e904211bbc64d
Junior Alves <junioralves.jav@gmail.com>
2023-08-24 18:26:28 -0300

    fix: corrige o nome da key do object persistedReducers

--- react/react-base/src/store/modules/reduxPersist.js
+++ react/react-base/src/store/modules/reduxPersist.js
@@ -4,7 +4,7 @@ import { persistReducer } from 'redux-persist';
 export default (reducers) => {
   const persistedReducers = persistReducer(
     {
-      key: 'TRACT-BASE',
+      key: 'REACT-BASE',
       storage,
       whitelist: ['example'], // O que será salvo
     },
